9fc37a5fc40de8dfd5e1a2c32d442321d8d03549
Erza Hasangjekaj <erza.hasangjekaj@uni-pr.edu>
2019-05-01 00:01:49 +0200

    Krijimi i klases Base64Decoder.

--- Base64_Encoder_Decoder/Base64_Encoder_Decoder/Form1.Designer.cs
+++ Base64_Encoder_Decoder/Base64_Encoder_Decoder/Form1.Designer.cs
@@ -148,7 +148,7 @@
             this.exitToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(826, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(834, 24);
             this.menuStrip1.TabIndex = 18;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -169,7 +169,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(826, 483);
+            this.ClientSize = new System.Drawing.Size(834, 483);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.txtDecoded);
